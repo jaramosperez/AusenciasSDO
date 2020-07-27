771e7884cf229540e1258bb3e652c2be8b39f3ed
Javier Ramos <jaramosperez85@gmail.com>
2020-07-27 15:05:54 -0400

    Corrigiendo algunos reportes

--- reportes/views.py
+++ reportes/views.py
@@ -19,7 +19,7 @@ class ReporteList(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'title': 'REPORTES'})
 
-############ REPORTES LICENCIAS ################
+# REPORTES LICENCIAS ################
 
 
 # REPORTE EXCEL LICENCIAS CORRESPONDIENTES AL MES ACTUAL
@@ -39,7 +39,7 @@ class ReporteLicenciasExcelMesActual(TemplateView):
         d3 = ws['D3']
         e3 = ws['E3']
         f3 = ws['F3']
-        ftr = Font(color=colors.RED, bold=True)
+        ftr = Font(color=colors.BLUE, bold=True)
         ftb = Font(color=colors.BLACK, bold=True, size=10)
         b1.font = ftr
         b3.font = ftb
@@ -69,13 +69,19 @@ class ReporteLicenciasExcelMesActual(TemplateView):
                 ws.cell(row=cont, column=5).font = Font(color=colors.BLACK, size=10)
                 ws.cell(row=cont, column=6).font = Font(color=colors.BLACK, size=10)
                 ws.cell(row=cont, column=2).value = ausencia.funcionario.run
-                ws.cell(row=cont, column=3).value = nombre
-                ws.cell(row=cont, column=4).value = ausencia.fecha_inicio.strftime('%d/%m/%Y')
-                ws.cell(row=cont, column=5).value = ausencia.fecha_termino.strftime('%d/%m/%Y')
+                ws.cell(row=cont, column=3).value = nombre.upper()
+                if ausencia.fecha_inicio is not None:
+                    ws.cell(row=cont, column=4).value = ausencia.fecha_inicio.strftime('%d/%m/%Y')
+                else:
+                    ws.cell(row=cont, column=4).value = 'No Ingresado'
+                if ausencia.fecha_termino is not None:
+                    ws.cell(row=cont, column=5).value = ausencia.fecha_termino.strftime('%d/%m/%Y')
+                else:
+                    ws.cell(row=cont, column=5).value = 'No Ingresado'
                 ws.cell(row=cont, column=6).value = ausencia.dias
                 cont += 1
 
-        nombre_archivo = "ReporteLicenciasMesActual.xlsx"
+        nombre_archivo = "ReporteLicenciasMesActual{}.xlsx".format(hoy)
         response = HttpResponse(content_type="application/ms-excel")
         content = "attachment; filename = {0}".format(nombre_archivo)
         response['Content-Disposition'] = content
@@ -102,7 +108,7 @@ class ReporteLicenciasExcelMesPasado(TemplateView):
         e3 = ws['E3']
         f3 = ws['F3']
         # TRABAJANDO EN EL REPORTE EXCEL
-        ftr = Font(color=colors.RED, bold=True)
+        ftr = Font(color=colors.BLUE, bold=True)
         ftb = Font(color=colors.BLACK, bold=True, size=10)
         b1.font = ftr
         b3.font = ftb
@@ -110,7 +116,7 @@ class ReporteLicenciasExcelMesPasado(TemplateView):
         d3.font = ftb
         e3.font = ftb
 
-        ws['B1'] = 'REPORTE DE AUSENCIAS GENERADO EL : ' + hoy.strftime('%d/%m/%Y')
+        ws['B1'] = 'REPORTE DE LICENCIAS GENERADO EL : ' + hoy.strftime('%d/%m/%Y')
 
         ws.merge_cells('B1:E1')
         ws['B3'] = 'RUN'
@@ -131,13 +137,19 @@ class ReporteLicenciasExcelMesPasado(TemplateView):
                 ws.cell(row=cont, column=5).font = Font(color=colors.BLACK, size=10)
                 ws.cell(row=cont, column=6).font = Font(color=colors.BLACK, size=10)
                 ws.cell(row=cont, column=2).value = ausencia.funcionario.run
-                ws.cell(row=cont, column=3).value = nombre
-                ws.cell(row=cont, column=4).value = ausencia.fecha_inicio.strftime('%d/%m/%Y')
-                ws.cell(row=cont, column=5).value = ausencia.fecha_termino.strftime('%d/%m/%Y')
+                ws.cell(row=cont, column=3).value = nombre.upper()
+                if ausencia.fecha_inicio is not None:
+                    ws.cell(row=cont, column=4).value = ausencia.fecha_inicio.strftime('%d/%m/%Y')
+                else:
+                    ws.cell(row=cont, column=4).value = 'No Ingresado'
+                if ausencia.fecha_termino is not None:
+                    ws.cell(row=cont, column=5).value = ausencia.fecha_termino.strftime('%d/%m/%Y')
+                else:
+                    ws.cell(row=cont, column=5).value = 'No Ingresado'
                 ws.cell(row=cont, column=6).value = ausencia.dias
                 cont += 1
 
-        nombre_archivo = "ReporteLicenciasMesAnterior.xlsx"
+        nombre_archivo = "ReporteLicencias_Mes_Anterior_{}.xlsx".format(hoy)
         response = HttpResponse(content_type="application/ms-excel")
         content = "attachment; filename = {0}".format(nombre_archivo)
         response['Content-Disposition'] = content
@@ -145,13 +157,14 @@ class ReporteLicenciasExcelMesPasado(TemplateView):
         return response
 
 
-# REPORTE EN EXCEL DE INDUCCIONES DEL PRESENTE AÑO
+# REPORTE EN EXCEL DE TODAS LAS INDUCCIONES DESDE EL 2016
 @method_decorator(login_required(), name='dispatch')
 class ReporteInduccionesExcel(TemplateView):
     def get(self, request, *args, **kwargs):
         hoy = datetime.date.today()
+        mes = mes = hoy.month
         annio = hoy.year
-        funcionarios = Funcionario.objects.filter(fecha_ingreso__year=annio)
+        funcionarios = Funcionario.objects.filter(fecha_ingreso__year=annio >= 2016)
         wb = Workbook()
         ws = wb.active
         # TRABAJANDO EN EL REPORTE EXCEL
@@ -161,7 +174,7 @@ class ReporteInduccionesExcel(TemplateView):
         d3 = ws['D3']
         e3 = ws['E3']
 
-        ftr = Font(color=colors.RED, bold=True, size=10)
+        ftr = Font(color=colors.BLUE, bold=True, size=10)
         ftb = Font(color=colors.BLACK, bold=True, size=10)
         b1.font = ftr
         b3.font = ftb
@@ -180,18 +193,26 @@ class ReporteInduccionesExcel(TemplateView):
         cont = 4
 
         for funcionario in funcionarios:
-            nombre = funcionario.nombres + ' ' + funcionario.apellido_paterno + ' ' + funcionario.apellido_materno
+            nombre = funcionario.nombres +\
+                     ' ' + funcionario.apellido_paterno +\
+                     ' ' + funcionario.apellido_materno
             ws.cell(row=cont, column=2).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=3).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=4).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=5).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=2).value = funcionario.run
-            ws.cell(row=cont, column=3).value = nombre
-            ws.cell(row=cont, column=4).value = funcionario.fecha_ingreso.strftime('%d/%m/%Y')
-            ws.cell(row=cont, column=5).value = funcionario.fecha_induccion.strftime('%d/%m/%Y')
+            ws.cell(row=cont, column=3).value = nombre.upper()
+            if funcionario.fecha_ingreso is not None:
+                ws.cell(row=cont, column=4).value = funcionario.fecha_ingreso.strftime('%d/%m/%Y')
+            else:
+                ws.cell(row=cont, column=4).value = 'No Ingresado'
+            if funcionario.fecha_induccion is not None:
+                ws.cell(row=cont, column=5).value = funcionario.fecha_induccion.strftime('%d/%m/%Y')
+            else:
+                ws.cell(row=cont, column=5).value = 'No Ingresado'
             cont += 1
 
-        nombre_archivo = "ReporteInduccionesExcel.xlsx"
+        nombre_archivo = "Reporte_Inducciones_Completo_{}.xlsx".format(hoy)
         response = HttpResponse(content_type="application/ms-excel")
         content = "attachment; filename = {0}".format(nombre_archivo)
         response['Content-Disposition'] = content
@@ -214,7 +235,7 @@ class ReporteTotalExcel(TemplateView):
         d3 = ws['D3']
         e3 = ws['E3']
 
-        ftr = Font(color=colors.RED, bold=True, size=10)
+        ftr = Font(color=colors.BLUE, bold=True, size=10)
         ftb = Font(color=colors.BLACK, bold=True, size=10)
         b1.font = ftr
         b3.font = ftb
@@ -233,18 +254,26 @@ class ReporteTotalExcel(TemplateView):
         cont = 4
 
         for funcionario in funcionarios:
-            nombre = funcionario.nombres + ' ' + funcionario.apellido_paterno + ' ' + funcionario.apellido_materno
+            nombre = funcionario.nombres + \
+                     ' ' + funcionario.apellido_paterno + \
+                     ' ' + funcionario.apellido_materno
             ws.cell(row=cont, column=2).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=3).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=4).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=5).font = Font(color=colors.BLACK, size=10)
             ws.cell(row=cont, column=2).value = funcionario.run
-            ws.cell(row=cont, column=3).value = nombre
-            ws.cell(row=cont, column=4).value = funcionario.fecha_ingreso.strftime('%d/%m/%Y')
-            ws.cell(row=cont, column=5).value = funcionario.fecha_induccion.strftime('%d/%m/%Y')
+            ws.cell(row=cont, column=3).value = nombre.upper()
+            if funcionario.fecha_ingreso is not None:
+                ws.cell(row=cont, column=4).value = funcionario.fecha_ingreso.strftime('%d/%m/%Y')
+            else:
+                ws.cell(row=cont, column=4).value = 'No Indica'
+            if funcionario.fecha_induccion is not None:
+                ws.cell(row=cont, column=5).value = funcionario.fecha_induccion.strftime('%d/%m/%Y')
+            else:
+                ws.cell(row=cont, column=4).value = 'No Indica'
             cont += 1
 
-        nombre_archivo = "ReporteTotal.xlsx"
+        nombre_archivo = "Reporte_Total_{}.xlsx".format(hoy)
         response = HttpResponse(content_type="application/ms-excel")
         content = "attachment; filename = {0}".format(nombre_archivo)
         response['Content-Disposition'] = content
